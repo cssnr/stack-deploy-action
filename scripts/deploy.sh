202d9b774ebcf66e7390aac39366d7d3101631cb
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-27 20:40:21 -0700

    Testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,17 +4,17 @@ set -ex
 mkdir -p ~/.ssh
 chmod 0700 ~/.ssh
 
-ssh-keyscan -p "${INPUT_PORT}" -H "${INPUT_HOST}" >> ~/.ssh/known_hosts
+#ssh-keyscan -p "${INPUT_PORT}" -H "${INPUT_HOST}" >> ~/.ssh/known_hosts
 ssh-keygen -q -f ~/.ssh/id_rsa -N "" -C "docker-stack-deploy-action"
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
 sshpass -p "${INPUT_PASS}" \
-    ssh-copy-id -p "${INPUT_PORT}" -i ~/.ssh/id_rsa \
+    ssh-copy-id -p "${INPUT_PORT}" -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" \
         "${INPUT_USER}@${INPUT_HOST}"
 
-ssh -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" docker info
+ssh -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" "docker info"
 
 docker context create remote --docker "host=ssh://${INPUT_USER}@${INPUT_HOST}:${INPUT_PORT}"
 docker context ls
