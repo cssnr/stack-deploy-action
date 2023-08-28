fb8fbd4b050bbf9dda4183ce91b7138d1231bb55
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-27 20:00:47 -0700

    Updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,43 +22,21 @@ chmod 0700 ~/.ssh
 echo -e "Host *\n    IdentityFile ~/.ssh/id_rsa\n    StrictHostKeyChecking no" > ~/.ssh/config
 ssh-keyscan -p "${INPUT_PORT}" -H "${INPUT_HOST}" >> ~/.ssh/known_hosts
 ssh-keygen -q -f ~/.ssh/id_rsa -N "" -C "stack-deploy-action"
-
-echo "--- 0 ---"
-echo "HOME: ${HOME}"
-echo -n "~: "
-echo ~
-echo "--- 0 ---"
-cat ~/.ssh/config
-echo "--- 0 ---"
-stat ~/.ssh
-echo "--- 0 ---"
-stat ~/.ssh/id_rsa
-
-sleep 3
-
-echo "--- 1 ---"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
-sleep 3
-
-echo "--- 2 ---"
 sshpass -p "${INPUT_PASS}" \
     ssh-copy-id -p "${INPUT_PORT}" -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" \
         "${INPUT_USER}@${INPUT_HOST}"
 
-echo "--- 3 ---"
-ssh -vv -p "${INPUT_PORT}" -o "StrictHostKeyChecking=no" "${INPUT_USER}@${INPUT_HOST}" whoami
+#ssh -vv -p "${INPUT_PORT}" -o "StrictHostKeyChecking=no" "${INPUT_USER}@${INPUT_HOST}" whoami
 ssh -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" whoami
 
-echo "--- 5 ---"
 export REMOTE="${INPUT_USER}@${INPUT_HOST}:${INPUT_PORT}"
 echo "REMOTE: ${REMOTE}"
 
-echo "--- 5 ---"
 export DOCKER_HOST="ssh://${REMOTE}"
 docker context create remote --docker "host=ssh://${REMOTE}"
 
-echo "--- 5 ---"
 docker context ls
 docker --context remote ps
