ab586d878b59f0fdf346cf1d0deb99cb50d9543d
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-31 19:30:24 -0700

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,7 @@ if [ -z "${INPUT_SSH_KEY}" ];then
             "${INPUT_USER}@${INPUT_HOST}"
 else
     echo "${INPUT_SSH_KEY}" > /root/.ssh/id_rsa
+    chmod 0600 /root/.ssh/id_rsa
     eval "$(ssh-agent -s)"
     ssh-add /root/.ssh/id_rsa
 fi
