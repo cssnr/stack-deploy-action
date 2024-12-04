e2a1f2163f023d0496c1e4b36af0cce0eef01298
StellarrW <132261669+StellarrW@users.noreply.github.com>
2024-12-04 23:20:40 +0000

    test

--- src/main.sh
+++ src/main.sh
@@ -11,7 +11,7 @@ function cleanup_trap() {
     fi
     if [ -z "${INPUT_SSH_KEY}" ];then
         echo -e "\u001b[35mCleaning Up authorized_keys on: ${INPUT_HOST}"
-        ssh -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" \
+        ssh -p "${INPUT_HOST_PORT}" "${INPUT_HOST_USERNAME}@${INPUT_HOST}" \
             "sed -i '/docker-stack-deploy-action/d' ~/.ssh/authorized_keys"
     fi
     if [[ "${_ST}" == "0" ]]; then
@@ -22,7 +22,7 @@ function cleanup_trap() {
 
 mkdir -p /root/.ssh
 chmod 0700 /root/.ssh
-ssh-keyscan -p "${INPUT_PORT}" -H "${INPUT_HOST}" >> /root/.ssh/known_hosts
+ssh-keyscan -p "${INPUT_HOST_PORT}" -H "${INPUT_HOST}" >> /root/.ssh/known_hosts
 
 if [ -z "${INPUT_SSH_KEY}" ];then
     echo -e "\u001b[36mCreating and Copying SSH Key to: ${INPUT_HOST}"
@@ -30,9 +30,9 @@ if [ -z "${INPUT_SSH_KEY}" ];then
     eval "$(ssh-agent -s)"
     ssh-add /root/.ssh/id_rsa
 
-    sshpass -p "${INPUT_PASS}" \
-        ssh-copy-id -p "${INPUT_PORT}" -i /root/.ssh/id_rsa \
-            "${INPUT_USER}@${INPUT_HOST}"
+    sshpass -p "${INPUT_HOST_PASSWORD}" \
+        ssh-copy-id -p "${INPUT_HOST_PORT}" -i /root/.ssh/id_rsa \
+            "${INPUT_HOST_USERNAME}@${INPUT_HOST}"
 else
     echo -e "\u001b[36mAdding SSH Key to SSH Agent"
     echo "${INPUT_SSH_KEY}" > /root/.ssh/id_rsa
@@ -43,10 +43,26 @@ fi
 
 trap cleanup_trap EXIT HUP INT QUIT PIPE TERM
 
-echo -e "\u001b[36mVerifying Docker and Setting Context."
-ssh -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" "docker info" > /dev/null
+login() {
+  echo "${INPUT_PASSWORD}" | docker login "${INPUT_REGISTRY}" -u "${INPUT_USERNAME}" --password-stdin
+}
 
-docker context create remote --docker "host=ssh://${INPUT_USER}@${INPUT_HOST}:${INPUT_PORT}"
+if [ -z "${INPUT_USERNAME+x}" ] || [ -z "${INPUT_PASSWORD+x}" ]; then
+  echo "Container Registry: No authentication provided"
+else
+  [ -z ${INPUT_REGISTRY+x} ] && export REGISTRY=""
+  if login > /dev/null 2>&1; then
+    echo "Container Registry: Logged in ${INPUT_REGISTRY} as ${INPUT_USERNAME}"
+  else
+    echo "Container Registry: Login to ${INPUT_REGISTRY} as ${INPUT_USERNAME} failed"
+    exit 1
+  fi
+fi
+
+echo -e "\u001b[36mVerifying Docker and Setting Context."
+ssh -p "${INPUT_HOST_PORT}" "${INPUT_HOST_USERNAME}@${INPUT_HOST}" "docker info" > /dev/null
+
+docker context create remote --docker "host=ssh://${INPUT_HOST_USERNAME}@${INPUT_HOST}:${INPUT_HOST_PORT}"
 docker context ls
 docker context use remote
 
@@ -61,4 +77,4 @@ if [ -n "${INPUT_ENV_FILE}" ];then
 fi
 
 echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME}"
-docker stack deploy -c "${INPUT_FILE}" "${INPUT_NAME}"
+docker stack deploy --with-registry-auth -c "${INPUT_FILE}" "${INPUT_NAME}"
