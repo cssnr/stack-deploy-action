b93f08703d0b8635fe5822262b05bfd056e63f8c
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-10 14:39:16 -0800

    Fix Docker Context Error

--- src/main.sh
+++ src/main.sh
@@ -46,9 +46,11 @@ trap cleanup_trap EXIT HUP INT QUIT PIPE TERM
 echo -e "\u001b[36mVerifying Docker and Setting Context."
 ssh -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" "docker info" > /dev/null
 
-docker context create remote --docker "host=ssh://${INPUT_USER}@${INPUT_HOST}:${INPUT_PORT}"
-docker context ls
+if ! docker context inspect remote >/dev/null 2>&1;then
+    docker context create remote --docker "host=ssh://${INPUT_USER}@${INPUT_HOST}:${INPUT_PORT}"
+fi
 docker context use remote
+docker context ls
 
 if [ -n "${INPUT_ENV_FILE}" ];then
     echo -e "\u001b[36mSourcing Environment File: \u001b[37;1m${INPUT_ENV_FILE}"
