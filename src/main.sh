6235fc0e0e09b5f734d9a0bda94feae11cfff955
Shane <6071159+smashedr@users.noreply.github.com>
2025-03-13 16:48:40 -0700

    Fix Error Capturing

--- src/main.sh
+++ src/main.sh
@@ -136,9 +136,11 @@ fi
 echo -e "::group::Deploying Docker ${_type} Stack: \u001b[36;1m${INPUT_NAME}"
 echo -e "\u001b[33;1m${COMMAND[*]}\n"
 exec 5>&1
+set +e
 # shellcheck disable=SC2034
 STACK_RESULTS=$( "${COMMAND[@]}" 2>&1 | tee >(cat >&5) ;exit "${PIPESTATUS[0]}" )
 EXIT_STATUS="$?"
+set -e
 echo "::endgroup::"
 
 ## Write Summary
